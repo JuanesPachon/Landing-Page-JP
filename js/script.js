e7abb46ab8da86ac0a6c775da45ce4cc47b5d269
Juanes <juanespachon1203@gmail.com>
2024-04-15 12:52:34 -0500

    Add Fifth section

--- js/script.js
+++ js/script.js
@@ -1,43 +1,46 @@
-
 /* Capture HTML Elements */
 
 const navbar = document.querySelector(".menu-container__icon");
-const overlay = document.querySelector(".overlay")
-const menuOptions = document.querySelectorAll(".scroll-links__link--mobile")
+const overlay = document.querySelector(".overlay");
+const menuOptions = document.querySelectorAll(".scroll-links__link--mobile");
 
-const scrollLinks = document.querySelectorAll(".scroll-links__link")
-const routes = document.querySelector("#third-section")
-const locationRoute = document.querySelector("#fourth-section")
+const scrollLinks = document.querySelectorAll(".scroll-links__link");
+const routes = document.querySelector("#third-section");
+const locationRoute = document.querySelector("#fourth-section");
+const plans = document.querySelector("#fifth-section");
 
-
-navbar.addEventListener("click", function() {
-
-    if (overlay.style.display === 'block') {
-        overlay.style.display = 'none';
-    } else {
-        overlay.style.display = 'block';
-    }
-})
-
-scrollLinks[0].addEventListener("click",() => {
-    routes.scrollIntoView({ behavior: 'smooth'})
+navbar.addEventListener("click", function () {
+  if (overlay.style.display === "block") {
+    overlay.style.display = "none";
+  } else {
+    overlay.style.display = "block";
+  }
 });
 
-scrollLinks[1].addEventListener("click",() => {
-    locationRoute.scrollIntoView({ behavior: 'smooth'})
+scrollLinks[0].addEventListener("click", () => {
+  routes.scrollIntoView({ behavior: "smooth" });
 });
 
-
-menuOptions.forEach(option => {                        /* We use ForEach due to in this case menuOptions is a nodelist */
-    option.addEventListener("click", function() {      /* that`s why we gotta add an event for each scroll link */
-        overlay.style.display = 'none';
-    });
+scrollLinks[1].addEventListener("click", () => {
+  locationRoute.scrollIntoView({ behavior: "smooth" });
 });
 
-menuOptions[0].addEventListener("click",() => {
-    routes.scrollIntoView({ behavior: 'smooth'})
+scrollLinks[2].addEventListener("click", () => {
+  plans.scrollIntoView({ behavior: "smooth" });
 });
 
-menuOptions[1].addEventListener("click",() => {
-    locationRoute.scrollIntoView({ behavior: 'smooth'})
+menuOptions.forEach((option) => {
+  /* We use ForEach due to in this case menuOptions is a nodelist */
+  option.addEventListener("click", function () {
+    /* that`s why we gotta add an event for each scroll link */ overlay.style.display =
+      "none";
+  });
+});
+
+menuOptions[0].addEventListener("click", () => {
+  routes.scrollIntoView({ behavior: "smooth" });
+});
+
+menuOptions[1].addEventListener("click", () => {
+  locationRoute.scrollIntoView({ behavior: "smooth" });
 });
